f02326dd7dadc40dc099f16c671ed7761449e21c
loufq <shloufq@gmail.com>
2016-01-01 21:45:49 +0800

    add base info

---
+++ ZhihuDailyPurify/BaseVC/BaseTableVC.h
@@ -0,0 +1,13 @@
+//
+//  BaseTableVC.h
+//  ZhihuNewsDaily
+//
+//  Created by loufq on 16/1/1.
+//  Copyright © 2016年 yeyy. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+@interface BaseTableVC : BaseVC
+
+@end
---
+++ ZhihuDailyPurify/BaseVC/BaseVC.m
@@ -0,0 +1,13 @@
+//
+//  BaseVC.m
+//  ZhihuNewsDaily
+//
+//  Created by loufq on 16/1/1.
+//  Copyright © 2016年 yeyy. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+@implementation BaseVC
+
+@end
